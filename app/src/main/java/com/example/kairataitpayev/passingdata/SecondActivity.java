67a96cc321013aab5c14a8a60b2d78ade4348d82
Kairat <kairat.other@gmail.com>
2014-09-11 17:55:12 +0600

    Initializing project

---
+++ app/src/main/java/com/example/kairataitpayev/passingdata/SecondActivity.java
@@ -0,0 +1,11 @@
+package com.example.kairataitpayev.passingdata;
+
+import android.app.Activity;
+
+/**
+ * Created by kairat.aitpayev on 9/11/2014.
+ */
+public class SecondActivity extends Activity
+{
+
+}
