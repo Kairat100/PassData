31ab8709b6c3ed88946f6b630dd7eeefc8ac17a2
Kairat <kairat.other@gmail.com>
2014-09-12 07:21:37 +0600

    Something

--- app/src/main/java/com/example/kairataitpayev/passingdata/SecondActivity.java
+++ app/src/main/java/com/example/kairataitpayev/passingdata/SecondActivity.java
@@ -1,11 +1,19 @@
 package com.example.kairataitpayev.passingdata;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
 
 /**
  * Created by kairat.aitpayev on 9/11/2014.
  */
 public class SecondActivity extends Activity
 {
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
 
+        Toast.makeText(this, getIntent().getStringExtra("str1"), Toast.LENGTH_SHORT).show();
+    }
 }
