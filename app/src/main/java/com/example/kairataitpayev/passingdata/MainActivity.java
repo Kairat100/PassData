40d239f8ea2ad275a19d17c0cf08fc5c992d0003
Kairat <kairat.other@gmail.com>
2014-09-12 09:58:26 +0600

    final updates

--- app/src/main/java/com/example/kairataitpayev/passingdata/MainActivity.java
+++ app/src/main/java/com/example/kairataitpayev/passingdata/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.kairataitpayev.passingdata;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,5 +17,33 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onClick(View v)
+    {
+        Intent i = new Intent("net.learn2develop.PassingDataSecondActivity");
 
+        i.putExtra("str1", "This is a string");
+        i.putExtra("age1", 25);
+
+        Bundle extras = new Bundle();
+        extras.putString("str2", "This is another string");
+        extras.putInt("age2", 35);
+
+        i.putExtras(extras);
+
+        startActivityForResult(i, 1);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == 1)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                Toast.makeText(this, Integer.toString(data.getIntExtra("age3", 0)), Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, data.getData().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+    }
 }
